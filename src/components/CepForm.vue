eee24f108985fe98b57dc803c3ed5b30cccb170e
Gabriel Dias <contatogbd@gmail.com>
2024-12-24 02:00:39 -0300

    adjusting input to only receive numbers, and ignore letters

--- src/components/CepForm.vue
+++ src/components/CepForm.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="flex flex-col gap-4">
-    <input
-      v-model="cep"
-      type="text"
-      placeholder="Digite o CEP"
-      class="border border-gray-300 rounded p-3 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-800"
-    />
-    <div class="flex gap-2">
+  <div class="space-y-4">
+    <div class="relative">
+      <input
+        v-model="cep"
+        @input="formatCep"
+        type="text"
+        maxlength="9"
+        placeholder="Digite o CEP"
+        class="w-full px-4 py-2 border rounded-lg bg-gray-100 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
+    <div class="flex space-x-2">
       <button
         @click="search"
-        class="flex-1 bg-blue-500 text-white p-3 rounded hover:bg-blue-600"
+        class="flex-1 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
       >
         Buscar
       </button>
       <button
         @click="clear"
-        class="flex-1 bg-gray-500 text-white p-3 rounded hover:bg-gray-600"
+        class="flex-1 bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600"
       >
         Limpar
       </button>
@@ -23,21 +27,30 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 
-export default defineComponent({
-  emits: ["search", "clear"],
-  setup(_, { emit }) {
-    const cep = ref("");
+const emit = defineEmits(["search", "clear"]);
+const cep = ref("");
 
-    const search = () => emit("search", cep.value);
-    const clear = () => {
-      cep.value = "";
-      emit("clear");
-    };
+const formatCep = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  let value = input.value.replace(/\D/g, "");
 
-    return { cep, search, clear };
-  },
-});
+  if (value.length > 5) {
+    value = value.substring(0, 5) + "-" + value.substring(5);
+  }
+
+  cep.value = value;
+};
+
+const search = () => {
+  const cleanCep = cep.value.replace(/\D/g, "");
+  emit("search", cleanCep);
+};
+
+const clear = () => {
+  cep.value = "";
+  emit("clear");
+};
 </script>
